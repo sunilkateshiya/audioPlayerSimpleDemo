a41e60232f9b4fe0da87894fe98dbddfffe6e22e
iFlame <iflame@administrators-device.local>
2017-04-22 13:17:21 +0530

    okk

--- audioPlayer/ViewController.swift
+++ audioPlayer/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer=AVAudioPlayer()
 
+    @IBAction func play(_ sender: Any)
+    {
+        player.play()
+    }
+    
+    @IBAction func pause(_ sender: Any)
+    {
+        player.pause()
+    }
+    
+    @IBAction func replay(_ sender: Any)
+    {
+        player.currentTime = 0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            let audiopath = Bundle.main.path(forResource: "Song", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf:NSURL(fileURLWithPath: audiopath!) as URL)
+            
+          }
+        catch
+        {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
